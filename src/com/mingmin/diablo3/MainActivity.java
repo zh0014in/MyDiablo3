0791654cdc084c47c35ba3c87f4ace86010414c5
Zhang Mingmin <zh0014in@e.ntu.edu.sg>
2014-08-22 16:46:36 +0800

    first add

---
+++ src/com/mingmin/diablo3/MainActivity.java
@@ -0,0 +1,182 @@
+package com.mingmin.diablo3;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import android.app.Activity;
+import android.app.ActionBar;
+import android.app.Fragment;
+import android.content.Context;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.os.Build;
+
+public class MainActivity extends Activity {
+
+	public static final String EXTRA_MESSAGE = "com.mingmin.diablo3.MESSAGE";
+	private TextView textView;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		
+
+		if (savedInstanceState == null) {
+			getFragmentManager().beginTransaction()
+					.add(R.id.container, new PlaceholderFragment()).commit();
+		}
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+
+		// Inflate the menu items for use in the action bar
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.menu.main_activity_actions, menu);
+		return super.onCreateOptionsMenu(menu);
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle presses on the action bar items
+		switch (item.getItemId()) {
+		case R.id.action_search:
+			// openSearch();
+			return true;
+		case R.id.action_settings:
+			// openSettings();
+			return true;
+		default:
+			return super.onOptionsItemSelected(item);
+		}
+	}
+
+	/**
+	 * A placeholder fragment containing a simple view.
+	 */
+	public static class PlaceholderFragment extends Fragment {
+
+		public PlaceholderFragment() {
+		}
+
+		@Override
+		public View onCreateView(LayoutInflater inflater, ViewGroup container,
+				Bundle savedInstanceState) {
+			View rootView = inflater.inflate(R.layout.fragment_main, container,
+					false);
+			return rootView;
+		}
+	}
+
+	/** Called when the user clicks the Send button */
+	public void sendMessage(View view) {
+		// Do something in response to button
+		Intent intent = new Intent(this, DisplayMessageActivity.class);
+		EditText editText = (EditText) findViewById(R.id.edit_message);
+		String message = editText.getText().toString();
+		intent.putExtra(EXTRA_MESSAGE, message);
+		startActivity(intent);
+	}
+
+	public void retriveInfo(View view) {
+		textView = (TextView) findViewById(R.id.textView1);
+		String uri = "http://tw.battle.net/api/d3/profile/Skyrim-6746/hero/28144494";
+		ConnectivityManager connMgr = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+		NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+		if (networkInfo != null && networkInfo.isConnected()) {
+			new DownloadWebpageTask().execute(uri);
+		} else {
+			textView.setText("No network connection available.");
+		}
+	}
+
+	// Uses AsyncTask to create a task away from the main UI thread. This task
+	// takes a
+	// URL string and uses it to create an HttpUrlConnection. Once the
+	// connection
+	// has been established, the AsyncTask downloads the contents of the webpage
+	// as
+	// an InputStream. Finally, the InputStream is converted into a string,
+	// which is
+	// displayed in the UI by the AsyncTask's onPostExecute method.
+	private class DownloadWebpageTask extends AsyncTask<String, Void, String> {
+		@Override
+		protected String doInBackground(String... urls) {
+
+			// params comes from the execute() call: params[0] is the url.
+			try {
+				return downloadUrl(urls[0]);
+			} catch (IOException e) {
+				return "Unable to retrieve web page. URL may be invalid.";
+			}
+		}
+
+		// onPostExecute displays the results of the AsyncTask.
+		@Override
+		protected void onPostExecute(String result) {
+			textView.setText(result);
+		}
+	}
+	
+	// Given a URL, establishes an HttpUrlConnection and retrieves
+	// the web page content as a InputStream, which it returns as
+	// a string.
+	private String downloadUrl(String myurl) throws IOException {
+	    InputStream is = null;
+	    // Only display the first 500 characters of the retrieved
+	    // web page content.
+	    int len = 500;
+	        
+	    try {
+	        URL url = new URL(myurl);
+	        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+	        conn.setReadTimeout(10000 /* milliseconds */);
+	        conn.setConnectTimeout(15000 /* milliseconds */);
+	        conn.setRequestMethod("GET");
+	        conn.setDoInput(true);
+	        // Starts the query
+	        conn.connect();
+	        int response = conn.getResponseCode();
+	        Log.d(EXTRA_MESSAGE, "The response is: " + response);
+	        is = conn.getInputStream();
+
+	        // Convert the InputStream into a string
+	        String contentAsString = readIt(is, len);
+	        return contentAsString;
+	        
+	    // Makes sure that the InputStream is closed after the app is
+	    // finished using it.
+	    } finally {
+	        if (is != null) {
+	            is.close();
+	        } 
+	    }
+	}
+	
+	// Reads an InputStream and converts it to a String.
+	public String readIt(InputStream stream, int len) throws IOException, UnsupportedEncodingException {
+	    Reader reader = null;
+	    reader = new InputStreamReader(stream, "UTF-8");        
+	    char[] buffer = new char[len];
+	    reader.read(buffer);
+	    return new String(buffer);
+	}
+}
